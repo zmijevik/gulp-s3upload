ed4a414705c89ff56161e7b1df082c9795325b23
Dwight Doane <dwight.doane@startupmedia.com>
2015-10-12 16:16:44 -0400

    example gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,24 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var app = require('./deploys3');
+var gulpS3Upload = require('./index.js');
 var gbug = require('gulp-debug');
+
+
 gulp.task('js', function() {
 	return gulp.src('./app/**/*', {buffer : false})
-		.pipe(gbug())
+		// .pipe(gbug())
 		.pipe(gulp.dest('./dist/'));
 });
 
 
 gulp.task('deploy', function() {
+	var root = 'dist';
 	return gulp.src('./dist/**/*', {buffer : false})
-		.pipe(gbug())
-		.pipe(app())
+		.pipe(gulpS3Upload({root: root}));
 });
+
+gulp.task('deploy.staging', function() {
+	var root = 'dist';
+	return gulp.src('./dist/**/*', {buffer : false})
+		.pipe(gulpS3Upload({root: root, bucketName: 'staging.advertising-client'}));
+})
